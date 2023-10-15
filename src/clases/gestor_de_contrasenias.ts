fe041ed8047ad5bd85e3764dd470582261dc3451
Anthony Mendoza <144974698+Deimuzh@users.noreply.github.com>
2023-10-15 00:17:10 -0500

    Revert "Anthony mendoza"

--- src/clases/gestor_de_contrasenias.ts
+++ src/clases/gestor_de_contrasenias.ts
@@ -2,31 +2,17 @@ import { Admin } from "./admin"
 import { Almacenamiento } from "./almacenamiento"
 
 export class GestorDeContrasenias {
-    private admin: Admin |null;
+    private admin: Admin;
     public version: string;
-    public db: Almacenamiento | null;
+    public db: Almacenamiento;
 
     constructor() {
         this.version = "1.0.0";
-        this.admin = null;
-        this.db = null;
-              
+        this.admin = new Admin("Pepito", "AssassinsCreen123");
+        this.db = new Almacenamiento();
     }
 
-    crearAdmin(nombre: string, clave: string): void {
-        this.admin = new Admin(nombre, clave);
-        console.log('Administrador creado:');
-     }
-    crearDB(): void { 
-        this.db = new Almacenamiento();
-        console.log();
-        }
-    
-    abrirDB(): void {
-        if (this.db) {
-            console.log('dato');
-          } else {
-            console.log('dato');
-        }
-     }
+    crearAdmin(nombre: string, clave: string): void { }
+    crearDB(): void { }
+    abrirDB(): void { }
 }
